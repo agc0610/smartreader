6ce89f1f69a0f0d7e95feb5ee0f472051f85ba57
Aimee Gonzalez-Cameron <aimeegonzalezcameron@Aimees-MacBook-Pro.local>
2021-07-18 12:22:27 +1000

    pseudocode draft

---
+++ get_input.py
@@ -0,0 +1,11 @@
+#pseudocode
+#a function to take an article as input and return first/last sentences 
+
+#get an input 
+#change into text
+#find the first paragraph 
+#get the first sentence of the paragraph and return it 
+#get the last sentence of the paragraph and return it 
+#repeat for the rest of the paragraphs 
+# --> limit to first page or two worth of paragraphs to test concept? 
+#break the loop after X paragraphs 
