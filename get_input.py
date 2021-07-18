cd204cbd6071f6333342cdd4d2c0d88f1de9792c
Aimee Gonzalez-Cameron <aimeegonzalezcameron@Aimees-MacBook-Pro.local>
2021-07-18 19:37:53 +1000

    add articles directory and work on coding pseudocode step 1

--- get_input.py
+++ get_input.py
@@ -1,11 +1,25 @@
-#pseudocode
 #a function to take an article as input and return first/last sentences 
 
-#get an input 
-#change into text
-#find the first paragraph 
-#get the first sentence of the paragraph and return it 
-#get the last sentence of the paragraph and return it 
-#repeat for the rest of the paragraphs 
+#pseudocode
+
+
+#3 find the first paragraph 
+#4 get the first sentence of the paragraph and return it 
+#5 get the last sentence of the paragraph and return it 
+#6 repeat for the rest of the paragraphs 
 # --> limit to first page or two worth of paragraphs to test concept? 
-#break the loop after X paragraphs 
+#7 break the loop after X paragraphs 
+
+#1 get an input 
+article = open('../articles/moredocsinscience.pdf')
+
+
+#2 change into text if needed 
+
+
+
+#adapt this for item 3 
+#for line in article: 
+#	line = line.rstrip() #strips right-hand side whitespace 
+#	if line.startswith(#can I use regex upper case with line break before it?)
+#		print(line)
