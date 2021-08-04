a3334eeca7d23780cfacad4cd629e28bcb0bc159
Aimee Gonzalez-Cameron <aimeegonzalezcameron@Aimees-MacBook-Pro.local>
2021-08-04 12:03:03 +1000

    saving current work to shut off computer

--- get_input.py
+++ get_input.py
@@ -1,25 +1,24 @@
 #a function to take an article as input and return first/last sentences 
+#of paragraphs on first 2 pages
 
-#pseudocode
+
+#1 get an input 
+article = open('../articles/moredocsinscience.pdf')
+
+#2 change into text if needed using PyPDF2 
+reader = PyPDF2.PdfFileReader(article)  
 
 
-#3 find the first paragraph 
+
+#3 find the first paragraph of page 1
+#
+for line in article: 
+	line = line.rstrip() #strips right-hand side whitespace 
+	if line.startswith(#can I use regex upper case with line break before it?)
+		print(line)
+
 #4 get the first sentence of the paragraph and return it 
 #5 get the last sentence of the paragraph and return it 
 #6 repeat for the rest of the paragraphs 
 # --> limit to first page or two worth of paragraphs to test concept? 
 #7 break the loop after X paragraphs 
-
-#1 get an input 
-article = open('../articles/moredocsinscience.pdf')
-
-
-#2 change into text if needed 
-
-
-
-#adapt this for item 3 
-#for line in article: 
-#	line = line.rstrip() #strips right-hand side whitespace 
-#	if line.startswith(#can I use regex upper case with line break before it?)
-#		print(line)
